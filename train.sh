b477f443cfd3ca096564c0ec9ab48f8e00689a10
Linfeng Song <freesunshine0316@gmail.com>
2021-09-07 11:38:17 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -7,12 +7,20 @@
 #        --restore_point experiments/coai_p1p2/pretrained_checkpoints/fold10.pt \
 #        --bert_path hfl/chinese-roberta-wwm-ext-large 
 
+#python train.py \
+#        --dataset coai \
+#        --lower_case \
+#        --fold 8 \
+#        --model coai_p1p2/fold_8 \
+#        --gpu 5 \
+#        --restore_point experiments/coai_p1p2/pretrained_checkpoints/fold8.pt \
+#        --bert_path hfl/chinese-roberta-wwm-ext-large 
+
+# regular training
 python train.py \
         --dataset coai \
         --lower_case \
-        --fold 8 \
-        --model coai_p1p2/fold_8 \
+        --fold "" \
+        --model coai/ \
         --gpu 5 \
-        --restore_point experiments/coai_p1p2/pretrained_checkpoints/fold8.pt \
         --bert_path hfl/chinese-roberta-wwm-ext-large 
-
